3ddf3618e054f40069e9c80e4724e938068cad31
haiksu7 <kitnissfil@yandex.ru>
2025-08-11 19:23:04 +0300

    smt

--- Main/main.py
+++ Main/main.py
@@ -3,11 +3,16 @@ from Catalog.catalog_views import router as catalog_router
 from Manga.manga_views import router as manga_router
 from User.user_views import router as user_router
 from Review.review_views import router as review_router
+from fastapi.middleware.cors import CORSMiddleware
 
 
 
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware, allow_origins=['*']
+)
 app.include_router(catalog_router)
 app.include_router(manga_router)
 app.include_router(user_router)
